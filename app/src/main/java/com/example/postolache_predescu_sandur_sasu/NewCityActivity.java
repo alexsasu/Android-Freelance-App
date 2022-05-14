a209d019744dba9405288e0340401be85975bf02
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2022-05-14 16:00:22 +0300

    backend part2
    with Raluca Sandu

---
+++ app/src/main/java/com/example/postolache_predescu_sandur_sasu/NewCityActivity.java
@@ -0,0 +1,65 @@
+package com.example.postolache_predescu_sandur_sasu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewCityActivity extends AppCompatActivity {
+
+    private EditText cityNameEdt;
+    private Button cityBtn;
+
+    public static final String EXTRA_ID = "com.example.postolache_predescu_sandur_sasu.EXTRA_ID";
+    public static final String EXTRA_CITY_NAME = "com.example.postolache_predescu_sandur_sasu.EXTRA_CITY_NAME";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_city);
+
+        cityNameEdt = findViewById(R.id.idEdtCityName);
+
+        cityBtn = findViewById(R.id.idBtnSaveCity);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(EXTRA_ID)) {
+
+            cityNameEdt.setText(intent.getStringExtra(EXTRA_CITY_NAME));
+
+        }
+        cityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String cityName = cityNameEdt.getText().toString();
+                if (cityName.isEmpty()) {
+                    Toast.makeText(NewCityActivity.this, "Please enter the valid city name.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                saveCity(cityName);
+            }
+        });
+    }
+
+    private void saveCity(String cityName) {
+
+        Intent data = new Intent();
+
+        data.putExtra(EXTRA_CITY_NAME, cityName);
+        int id = getIntent().getIntExtra(EXTRA_ID, -1);
+        if (id != -1) {
+            data.putExtra(EXTRA_ID, id);
+        }
+
+        setResult(RESULT_OK, data);
+
+        Toast.makeText(this, "city has been saved successfully. ", Toast.LENGTH_SHORT).show();
+    }
+}
+
---
+++ app/src/main/java/com/example/postolache_predescu_sandur_sasu/NewJobActivity.java
@@ -0,0 +1,58 @@
+package com.example.postolache_predescu_sandur_sasu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewJobActivity extends AppCompatActivity {
+
+    private EditText jobNameEdt;
+    private Button jobBtn;
+
+    public static final String EXTRA_ID = "com.example.postolache_predescu_sandur_sasu.EXTRA_ID";
+    public static final String EXTRA_JOB_NAME = "com.example.postolache_predescu_sandur_sasu.EXTRA_JOB_NAME";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_job);
+        
+        jobNameEdt = findViewById(R.id.idEdtJobName);
+        jobBtn = findViewById(R.id.idBtnSaveJob);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(EXTRA_ID)) {
+            jobNameEdt.setText(intent.getStringExtra(EXTRA_JOB_NAME));
+        }
+
+        jobBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String jobName = jobNameEdt.getText().toString();
+                if (jobName.isEmpty()) {
+                    Toast.makeText(NewJobActivity.this, "Please enter the valid job name.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                saveJob(jobName);
+            }
+        });
+    }
+
+    private void saveJob(String jobName) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_JOB_NAME, jobName);
+        int id = getIntent().getIntExtra(EXTRA_ID, -1);
+        if (id != -1) {
+
+            data.putExtra(EXTRA_ID, id);
+        }
+
+        setResult(RESULT_OK, data);
+        Toast.makeText(this, "Job has been saved successfully. ", Toast.LENGTH_SHORT).show();
+    }
+}
