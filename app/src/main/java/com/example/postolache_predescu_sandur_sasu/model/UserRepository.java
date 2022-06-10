a710f34c3340a6f178af5c84c7e85a4ff0d6b8e3
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2022-06-10 23:16:14 +0300

    Update UserRepository.java

--- app/src/main/java/com/example/postolache_predescu_sandur_sasu/model/UserRepository.java
+++ app/src/main/java/com/example/postolache_predescu_sandur_sasu/model/UserRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class UserRepository {
 
     private DaoUser dao;
+    
     private LiveData<List<User>> allUsers;
 
     public UserRepository(Application application) {
