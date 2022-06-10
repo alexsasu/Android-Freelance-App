cc606b2cf234a613b98a416383d62bc42d383727
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2022-06-10 23:16:43 +0300

    Update UserDatabase.java

--- app/src/main/java/com/example/postolache_predescu_sandur_sasu/model/UserDatabase.java
+++ app/src/main/java/com/example/postolache_predescu_sandur_sasu/model/UserDatabase.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {User.class}, version = 1)
